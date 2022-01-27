b834e1f8c0df7a70f63777038714f3516d097476
Ash3aibi <97199464+Ash3aibi@users.noreply.github.com>
2022-01-26 17:07:46 -0800

    main.c
    updated

--- Lab_0/main.c
+++ Lab_0/main.c
@@ -3,7 +3,8 @@
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
-    
+         printf(“Now live on GitHub!\n”);
+	
     for(int i = 0; i < argc; i++ )
     {
         printf( "%d - %s\n", i, argv[i] );
